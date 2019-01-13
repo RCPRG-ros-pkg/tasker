b2d7c2a05eaabf0d8bda77cc400fc8a4e1ba62f7
dudekw <wojciechsbox@gmail.com>
2019-01-13 22:01:54 +0100

    adds pddl-planner compile script

---
+++ compile-pddl-planner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo pip install pddlpy
+sudo apt-get install scons
+git clone https://github.com/KCL-Planning/VAL
+cd VAL
+make clean
+make
+cd ..
+git clone https://github.com/aig-upf/universal-pddl-parser.git
+cd universal-pddl-parser
+scons
+cd ..
+git clone https://github.com/aig-upf/temporal-planning.git
+cd temporal-planning
+python fd_copy/build.py release64
+scons
+cd ..
