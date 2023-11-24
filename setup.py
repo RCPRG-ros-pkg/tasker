f3d606cbd2fd858d62dbf51ec446cddc3c6280be
Adam Krawczyk <adam.krawczyk@robotec.ai>
2023-11-24 10:55:26 +0100

    Remove duplicates and bin
    Signed-off-by: Adam Krawczyk <adam.krawczyk@robotec.ai>

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'tasker'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['TaskER'],
-    package_dir={'': 'src'},
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='you@yourdomain.tld',
+    description='The TaskER package',
+    license='BSD',
+    tests_require=['pytest'],
+    scripts=['bin/task_harmonizer'],
 )
-
-setup(**setup_args)
